f5d1fc9409d4b59403b0946dd83905f505a3ec51
artiehumphreys <artieh2003@gmail.com>
2024-07-09 14:14:42 -0400

    feat: class support for conversion

--- convert/bvh_to_glb.py
+++ convert/bvh_to_glb.py
@@ -3,97 +3,98 @@ import os
 import mathutils # type: ignore
 from pathlib import Path
 
-scale = 1.25
-output_path = "babylon_viewer/output"
-file_name = ""
-player_ids: dict[int, str] = {}
-teams: set[str] = set()
+class bvh_to_glb:
+    def __init__(self, output_path = "babylon_viewer/output"):
+        self.output_path = output_path
+        self.file_name = ""
+        self.player_ids: dict[int, str] = {}
+        self.teams: set[str] = set()
+        self.scale = 1.25
 
-def process_teams_ids(filename):
-    global player_ids, teams
-    split = filename.split('_')[1]
-    if not split[1] in teams:
-        teams.add(split[1])
-    if not split[2] in player_ids:
+    def process_teams_ids(self, filename):
+        global player_ids, teams
+        split = filename.split('_')[1]
+        if not split[1] in teams:
+            teams.add(split[1])
         player_ids[split[2]] = ""
 
+    # def read_csv(self,filename = r"players 1.csv"):
 
-
-def create_sphere_at_bone(bone, armature):
-    if bone.name == 'lWrist' or bone.name == 'rWrist':
-        bpy.ops.mesh.primitive_uv_sphere_add(radius=bone.head_radius / scale, location=bone.head_local)
+    def create_sphere_at_bone(self, bone, armature):
+        if bone.name == 'lWrist' or bone.name == 'rWrist':
+            bpy.ops.mesh.primitive_uv_sphere_add(radius=bone.head_radius / self.scale, location=bone.head_local)
+            sphere = bpy.context.object
+            sphere.scale = ((bone.head_local - bone.tail_local).length * 10, 1, 1)
+        else:    
+            bpy.ops.mesh.primitive_uv_sphere_add(radius=bone.head_radius, location=bone.head_local)
         sphere = bpy.context.object
-        sphere.scale = ((bone.head_local - bone.tail_local).length * 10, 1, 1)
-    else:    
-        bpy.ops.mesh.primitive_uv_sphere_add(radius=bone.head_radius, location=bone.head_local)
-    sphere = bpy.context.object
-    sphere.name = f"sphere_{bone.name}_{file_name}"
-    if bone.name == 'baseHead':
-        head_height = 0.15
-        sphere.location += mathutils.Vector((0, 0, head_height))
-        sphere.scale = (scale, scale, scale)
+        sphere.name = f"sphere_{bone.name}_{file_name}"
+        if bone.name == 'baseHead':
+            head_height = 0.15
+            sphere.location += mathutils.Vector((0, 0, head_height))
+            sphere.scale = (self.scale, self.scale, self.scale)
+
+        mod = sphere.modifiers.new(name="Armature", type="ARMATURE")
+        mod.object = armature
+        sphere.parent = armature
+        sphere.parent_type = "BONE"
+        sphere.parent_bone = bone.name
 
 
-    mod = sphere.modifiers.new(name="Armature", type="ARMATURE")
-    mod.object = armature
-    sphere.parent = armature
-    sphere.parent_type = "BONE"
-    sphere.parent_bone = bone.name
+    def create_cone_arm(self, bone, armature):
+        parent = bone.parent
+
+        cone_vector = bone.head_local - parent.head_local
+        cone_length = cone_vector.length
+        cone_direction = cone_vector.normalized()
+
+        cone_midpoint_world = (parent.head_local + bone.head_local) / 2
+
+        bpy.ops.mesh.primitive_cone_add(radius1=bone.head_radius / self.scale, radius2=bone.tail_radius / self.scale, depth=cone_length, location=cone_midpoint_world)
+        cone = bpy.context.object
+        cone.name = f"Cone_{parent.name}_to_{bone.name}_{file_name}"
+
+        rotation = cone_direction.to_track_quat('Z', 'Y').to_euler()
+        cone.rotation_euler = rotation
+
+        cone.parent = armature
+        cone.parent_type = "BONE"
+        cone.parent_bone = parent.name
 
 
-def create_cone_arm(bone, armature):
-    parent = bone.parent
+    def convert_bvh_to_glb(self, directory, output_name):
+        global file_name
+        bpy.ops.wm.read_factory_settings(use_empty=True)
 
-    cone_vector = bone.head_local - parent.head_local
-    cone_length = cone_vector.length
-    cone_direction = cone_vector.normalized()
+        bpy.ops.wm.obj_import(filepath="Basketball_court.obj")
 
-    cone_midpoint_world = (parent.head_local + bone.head_local) / 2
+        for filename in os.listdir(directory):
+            if not filename.endswith(".bvh"):
+                continue
+            file_name = Path(filename).stem
+            self.process_teams_ids(file_name)
 
-    bpy.ops.mesh.primitive_cone_add(radius1=bone.head_radius / scale, radius2=bone.tail_radius / scale, depth=cone_length, location=cone_midpoint_world)
-    cone = bpy.context.object
-    cone.name = f"Cone_{parent.name}_to_{bone.name}_{file_name}"
+            bvh_path = os.path.join(directory, filename)
+            bpy.ops.import_anim.bvh(filepath=bvh_path)
+            bpy.context.scene.render.fps = 60
 
-    rotation = cone_direction.to_track_quat('Z', 'Y').to_euler()
-    cone.rotation_euler = rotation
+            armature = bpy.context.object
+            if armature.type != "ARMATURE":
+                continue
+            bpy.context.view_layer.objects.active = armature
 
-    cone.parent = armature
-    cone.parent_type = "BONE"
-    cone.parent_bone = parent.name
+            for bone in armature.pose.bones:
+                bone.bone.use_local_location = True
+                bone.bone.use_relative_parent = True
+                self.create_sphere_at_bone(bone.bone, armature)
+                if bone.bone.parent:
+                    self.create_cone_arm(bone.bone, armature)
 
+        bpy.ops.object.select_all(action="SELECT")
 
-def convert_bvh_to_glb(directory, output_name):
-    global file_name
-    bpy.ops.wm.read_factory_settings(use_empty=True)
-
-    bpy.ops.wm.obj_import(filepath="Basketball_court.obj")
-
-    for filename in os.listdir(directory):
-        if not filename.endswith(".bvh"):
-            continue
-        file_name = Path(filename).stem
-        process_teams_ids(file_name)
-
-        bvh_path = os.path.join(directory, filename)
-        bpy.ops.import_anim.bvh(filepath=bvh_path)
-        bpy.context.scene.render.fps = 60
-
-        armature = bpy.context.object
-        if armature.type != "ARMATURE":
-            continue
-        bpy.context.view_layer.objects.active = armature
-
-        for bone in armature.pose.bones:
-            bone.bone.use_local_location = True
-            bone.bone.use_relative_parent = True
-            create_sphere_at_bone(bone.bone, armature)
-            if bone.bone.parent:
-                create_cone_arm(bone.bone, armature)
-
-    bpy.ops.object.select_all(action="SELECT")
-
-    bpy.ops.export_scene.gltf(filepath=output_name, export_format="GLB")
+        bpy.ops.export_scene.gltf(filepath=output_name, export_format="GLB")
 
 
 if __name__ == "__main__":
-    convert_bvh_to_glb("output_BVH", output_path)
+    converter = bvh_to_glb()
+    converter.convert_bvh_to_glb("output_BVH", converter.output_path)
