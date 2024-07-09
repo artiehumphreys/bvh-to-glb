894519f3bfe683e719f5af0a005a83e6f04fdf62
artiehumphreys <artieh2003@gmail.com>
2024-07-09 13:33:13 -0400

    feat: enhanced support for chrome, ensuring that only one localhost tab is up at a time

--- run.sh
+++ run.sh
@@ -1,13 +1,17 @@
 #!/bin/sh
 cd $(pwd) || exit
+brew list python &>/dev/null || brew install python
+brew list chrome-cli &>/dev/null || brew install chrome-cli
 python -m venv venv
 source venv/bin/activate
-python -m pip install --upgrade pip
+python -m pip install --upgrade pip &>/dev/null
 python -m pip install -r requirements.txt
 venv/bin/python convert/bvh_to_glb.py
 cd babylon_viewer
 lsof -t -i:5500 | xargs kill -9 2>/dev/null || true
+id=$(chrome-cli list links | grep 'localhost:5500' | awk -F'[:\\]]' '{print $2}' | awk '{print $1}')
 python -m http.server 5500 &
 sleep 1
+chrome-cli close -t $id
 open http://localhost:5500/
 cd ..
