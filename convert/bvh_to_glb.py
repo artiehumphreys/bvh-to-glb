f8a26ef353fa7ff90c2e4b6276540ab979f9166e
artiehumphreys <artieh2003@gmail.com>
2024-07-09 15:05:27 -0400

    feat: get team method and setting color based on team

--- convert/bvh_to_glb.py
+++ convert/bvh_to_glb.py
@@ -1,48 +1,64 @@
 import bpy  # type: ignore
 import os
-import mathutils # type: ignore
+import mathutils  # type: ignore
 from pathlib import Path
 import csv
 
+
 class bvh_to_glb:
-    def __init__(self, output_path = "babylon_viewer/output"):
+    def __init__(self, dir="output_BVH", output_path="babylon_viewer/output"):
         self.output_path: str = output_path
         self.filename: str = ""
         self.player_ids: dict[int, str] = {}
         self.teams: set[str] = set()
         self.scale: float = 1.25
+        self.dir = dir
+        self.process_players(self.dir)
+
+    def get_team(self):
+        split = self.filename.split("_")
+        return split[1]
 
     def process_players(self, directory):
         for file in os.listdir(directory):
             if not file.endswith(".bvh"):
                 continue
             filename = Path(file).stem
-            split = filename.split('_')
+            split = filename.split("_")
             if not split[1] in self.teams:
                 self.teams.add(split[1])
             self.player_ids[int(split[2])] = ""
         self.read_csv()
 
-    def read_csv(self, file = r"players 1.csv"):
-        with open(file, mode='r') as csvfile:
+    def read_csv(self, file=r"players 1.csv"):
+        with open(file, mode="r") as csvfile:
             reader = csv.DictReader(csvfile)
             for row in reader:
-                player_id = int(row['player_id'])
+                player_id = int(row["player_id"])
                 if player_id not in self.player_ids:
                     continue
-                self.player_ids[player_id] = row['player_name']
+                self.player_ids[player_id] = row["player_name"]
 
+    def assign_team(self, shape):
+        team = self.get_team()
+        mat = bpy.data.materials.new(name=f"Material_{team}")
+        mat.diffuse_color = (1, 1, 1, 1)
+        shape.data.materials.append(mat)
 
     def create_sphere_at_bone(self, bone, armature):
-        if bone.name == 'lWrist' or bone.name == 'rWrist':
-            bpy.ops.mesh.primitive_uv_sphere_add(radius=bone.head_radius / self.scale, location=bone.head_local)
+        if bone.name == "lWrist" or bone.name == "rWrist":
+            bpy.ops.mesh.primitive_uv_sphere_add(
+                radius=bone.head_radius / self.scale, location=bone.head_local
+            )
             sphere = bpy.context.object
             sphere.scale = ((bone.head_local - bone.tail_local).length * 10, 1, 1)
-        else:    
-            bpy.ops.mesh.primitive_uv_sphere_add(radius=bone.head_radius, location=bone.head_local)
+        else:
+            bpy.ops.mesh.primitive_uv_sphere_add(
+                radius=bone.head_radius, location=bone.head_local
+            )
         sphere = bpy.context.object
         sphere.name = f"sphere_{bone.name}_{self.filename}"
-        if bone.name == 'baseHead':
+        if bone.name == "baseHead":
             head_height = 0.15
             sphere.location += mathutils.Vector((0, 0, head_height))
             sphere.scale = (self.scale, self.scale, self.scale)
@@ -52,7 +68,7 @@ class bvh_to_glb:
         sphere.parent = armature
         sphere.parent_type = "BONE"
         sphere.parent_bone = bone.name
-
+        self.assign_team(sphere)
 
     def create_cone_arm(self, bone, armature):
         parent = bone.parent
@@ -63,32 +79,34 @@ class bvh_to_glb:
 
         cone_midpoint_world = (parent.head_local + bone.head_local) / 2
 
-        bpy.ops.mesh.primitive_cone_add(radius1=bone.head_radius / self.scale, radius2=bone.tail_radius / self.scale, depth=cone_length, location=cone_midpoint_world)
+        bpy.ops.mesh.primitive_cone_add(
+            radius1=bone.head_radius / self.scale,
+            radius2=bone.tail_radius / self.scale,
+            depth=cone_length,
+            location=cone_midpoint_world,
+        )
         cone = bpy.context.object
         cone.name = f"Cone_{parent.name}_to_{bone.name}_{self.filename}"
 
-        rotation = cone_direction.to_track_quat('Z', 'Y').to_euler()
+        rotation = cone_direction.to_track_quat("Z", "Y").to_euler()
         cone.rotation_euler = rotation
 
         cone.parent = armature
         cone.parent_type = "BONE"
         cone.parent_bone = parent.name
+        self.assign_team(cone)
 
-
-    def convert_bvh_to_glb(self, directory, output_name):
-        self.process_players(directory)
-        print(self.player_ids)
-
+    def convert_bvh_to_glb(self, output_name):
         bpy.ops.wm.read_factory_settings(use_empty=True)
 
         bpy.ops.wm.obj_import(filepath="Basketball_court.obj")
 
-        for file in os.listdir(directory):
+        for file in os.listdir(self.dir):
             if not file.endswith(".bvh"):
                 continue
             self.filename = Path(file).stem
 
-            bvh_path = os.path.join(directory, file)
+            bvh_path = os.path.join(self.dir, file)
             bpy.ops.import_anim.bvh(filepath=bvh_path)
             bpy.context.scene.render.fps = 60
 
@@ -111,4 +129,4 @@ class bvh_to_glb:
 
 if __name__ == "__main__":
     converter = bvh_to_glb()
-    converter.convert_bvh_to_glb("output_BVH", converter.output_path)
+    converter.convert_bvh_to_glb(converter.output_path)
