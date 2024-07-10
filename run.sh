944769358a127e2321915f1f67ecb84f35bdd0f2
artiehumphreys <artieh2003@gmail.com>
2024-07-10 13:35:41 -0400

    fix: fixed shell script

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 cleanup() {
     lsof -t -i:5500 | xargs kill -9 2>/dev/null || true
     exit 0
@@ -9,13 +9,16 @@ cd $(pwd) || exit
 brew list python &>/dev/null || brew install python
 brew list chrome-cli &>/dev/null || brew install chrome-cli
 python -m venv venv
-source venv/bin/activate
-python -m pip install --upgrade pip &>/dev/null
-python -m pip install -r requirements.txt
-venv/bin/python convert/bvh_to_glb.py
+source venv/bin/activate || { echo "Failed to activate virtual environment"; exit 1; }
+p="venv/bin/python"
+$p -m pip install --upgrade pip &>/dev/null
+$p -m pip install -r requirements.txt
+$p convert/bvh_to_glb.py
 cd babylon_viewer
 id=$(chrome-cli list links | grep 'localhost:5500' | awk -F'[:\\]]' '{print $2}' | awk '{print $1}')
-python -m http.server 5500 &
+p="../venv/bin/python"
+echo "Starting HTTP server..."
+$p -m http.server 5500 &
 HTTP_SERVER_PID=$!
 sleep 0.5
 if [ -z "$id" ]; then
